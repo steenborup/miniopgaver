fdcf3c75ae0d744190d8a45d00092b724200bb3e
Steen Borup <steenborup@gmail.com>
2016-11-08 13:09:00 +0100

    Opgave 1

---
+++ miniopgaver/miniopgaver/Opg11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Miniopgaver
+{
+    public class Opgave
+    {
+        public static void Main()
+        {
+            Opgave1();                    
+        }
+
+        public static void Opgave1()
+        {
+            Debug.WriteLine("Hello");
+            Debug.WriteLine("Steen");
+        }
+    }
+}
